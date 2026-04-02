bc6c51cdd3ea92e8b93fa93915333e7d7895d008
Gijn2 <167487112+Gijn2@users.noreply.github.com>
2026-04-03 00:59:59 +0900

    2026-04-03
    00:57

--- company-homepage/src/main/java/com/rang/companyhomepage/repository/MandaoReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rang.companyhomepage.repository;
-
-import com.rang.companyhomepage.domain.MandaoReservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface MandaoReservationRepository extends JpaRepository<MandaoReservation, Long> {
-}
