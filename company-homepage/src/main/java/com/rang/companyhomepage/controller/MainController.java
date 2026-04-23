cf77782f86858b2786e78a76052da6aa4926d7ec
Gijn2 <167487112+Gijn2@users.noreply.github.com>
2026-04-23 20:49:25 +0900

    2026-04-23
    20:49

--- company-homepage/src/main/java/com/rang/companyhomepage/controller/MainController.java
+++ company-homepage/src/main/java/com/rang/companyhomepage/controller/MainController.java
@@ -1,39 +1,39 @@
-package com.rang.companyhomepage.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MainController {
-
-
-    @GetMapping({"/", "/index"})
-    public String index() {
-        // src/main/resources/templates/index.html 호출
-        return "index";
-    }
-
-    @GetMapping("/projectR")
-    public String projectR() {
-        // src/main/resources/templates/projectR.html 호출
-        return "projectR";
-    }
-
-    @GetMapping("/projectM")
-    public String projectM() {
-        // src/main/resources/templates/projectM.html 호출
-        return "projectM";
-    }
-
-    @GetMapping("/mandao")
-    public String mandao() {
-        // src/main/resources/templates/mandao.html 호출
-        return "mandao";
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        // src/main/resources/templates/test.html 호출
-        return "test";
-    }
-}
+//package com.rang.companyhomepage.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class MainController {
+//
+//
+//    @GetMapping({"/", "/index"})
+//    public String index() {
+//        // src/main/resources/templates/index.html 호출
+//        return "index";
+//    }
+//
+//    @GetMapping("/projectR")
+//    public String projectR() {
+//        // src/main/resources/templates/projectR.html 호출
+//        return "projectR";
+//    }
+//
+//    @GetMapping("/projectM")
+//    public String projectM() {
+//        // src/main/resources/templates/projectM.html 호출
+//        return "projectM";
+//    }
+//
+//    @GetMapping("/mandao")
+//    public String mandao() {
+//        // src/main/resources/templates/mandao.html 호출
+//        return "mandao";
+//    }
+//
+//    @GetMapping("/communityRPoll")
+//    public String test() {
+//        // src/main/resources/templates/test.html 호출
+//        return "communityRPoll";
+//    }
+//}
